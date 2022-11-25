43c0f739f949409548278f48bc462747eb4807b2
Quan Bui <quanbui021001@gmail.com>
2022-11-25 18:44:47 +0200

    xx

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = (props) => {
     return (
         <React.Fragment>
             <header className={classes.header}>
-                <h1>React Meals</h1>
+                <h1>Happy Meals</h1>
                 <HeaderCartButton open={props.open}/>
             </header>
             <div className={classes['main-image']}>
